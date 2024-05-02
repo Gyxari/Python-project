2d7db1231591354c733b15a28e907c00e722fa37
Jesse Kykyri <ab6894@student.jamk.fi>
2024-05-02 10:52:22 +0300

    added more views, created index.js and profile.html

--- views.py
+++ views.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views=Blueprint(__name__,"views")
 
 @views.route("/")
 def home():
-    return "Home Page"
+    return render_template("index.html")
+
+@views.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'Kyksä', 'status': 'Owner'})
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home ():
+    return redirect(url_for("views.home"))
